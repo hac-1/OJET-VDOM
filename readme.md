9e34a61c0af5a6b97448bd00d220fbcfd7773514
HAC <harifspa19@gmail.com>
2025-01-12 16:15:09 +0530

    First Learning Path End

---
+++ readme.md
@@ -0,0 +1,18 @@
+Create a new basic app using `npx @oracle/ojet-cli create JET-Virtual-DOM-app --template=basic --vdom`<br>
+There are other templates also available
+
+Install (if existing OJET) by using `npm install`
+
+Start with `npx ojet serve`
+
+Test with `npm test`
+
+Deploy build can be created using `npx ojet build web --release`
+
+
+Useful Links
+- https://docs.oracle.com/en/middleware/developer-tools/jet/17.1/virtual-dom-apps-learning-paths.html
+- https://docs.oracle.com/en/middleware/developer-tools/jet/17.1/vdom/index.html
+- https://www.oracle.com/webfolder/technetwork/jet/public_samples/JET-VComponent-Tutorial-VDOM/public_html/index.html?ojr=welcome
+- https://www.oracle.com/webfolder/technetwork/jet/jetCookbook.html
+- https://docs.oracle.com/en/middleware/developer-tools/jet/17.1/reference-api/index.html
---
+++ src/components/content/__tests__/content.spec.tsx
@@ -0,0 +1,34 @@
+import { h } from "preact";
+import { render } from "@testing-library/preact";
+import { Content } from "../index";
+
+jest.mock("../../store_data.json", () => [
+    { id: 1, name: "Baseball", short_desc: "A bat-and-ball game" },
+    { id: 2, name: "Soccer", short_desc: "A football game" },
+    { id: 3, name: "Bicycling", short_desc: "Equipment we carry for biking enthusiasts." },
+]);
+
+jest.mock("ojs/ojmutablearraydataprovider", () => {
+    return jest.fn(() => ({
+        containsKeys: jest.fn(),
+        fetchByKeys: jest.fn(),
+        fetchByOffset: jest.fn(),
+        fetchFirst: jest.fn(() => ({
+            [Symbol.asyncIterator]() {
+                return {
+                    next: () => Promise.resolve({ value: [], done: true }),
+                };
+            },
+        })),
+    }));
+});
+
+describe("Test suite for Content component", () => {
+
+    test("Renders Product Information header with a H1 tag", () => {
+        const { getByText } = render(<Content />);
+        const headerElement = getByText("Product Information");
+        expect(headerElement.tagName).toBe("H1");
+    });
+
+})
