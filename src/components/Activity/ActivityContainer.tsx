02b4fa0e28610968c5dcf1a40a4a75325c602935
HAC <harifspa19@gmail.com>
2025-01-25 13:19:02 +0530

    Creationg of Master Details

--- src/components/Activity/ActivityContainer.tsx
+++ src/components/Activity/ActivityContainer.tsx
@@ -1,17 +1,62 @@
+import { ojListView } from "ojs/ojlistview";
+import MutableArrayDataProvider = require("ojs/ojmutablearraydataprovider");
 import { h, ComponentProps } from "preact";
 
-const ActivityContainer = () => {
+type Activity = {
+    id: number;
+    name: string;
+    short_desc: string;
+};
+
+type Props = {
+    data: MutableArrayDataProvider<Activity['id'], Activity>
+    value?: String
+}
+
+const listItemRenderer = (item: ojListView.ItemTemplateContext) => {
+    let image = item.data.image.replace('css', 'styles');
+    return (
+        <div class="oj-flex no-wrap">
+            <span
+                class="demo-thumbnail oj-flex-item"
+                style={'background-image:url(' + image + ')'}></span>
+            <div class="demo-content oj-flex-item">
+                <div>
+                    <strong>{item.data.name}</strong>
+                </div>
+                <span class="demo-metadata">{item.data.short_desc}</span>
+            </div>
+        </div>
+    );
+}
+
+type ListViewProps = ComponentProps<'oj-list-view'>
+const gridlinesItemVisible: ListViewProps['gridlines'] = { item: 'visible' };
+const scrollPolicyOpts: ListViewProps['scrollPolicyOptions'] = { fetchSize: 5 };
+
+const ActivityContainer = (props: Props) => {
     return (
         <div id="activitiesContainer" class="oj-flex-item oj-bg-info-30 oj-sm-only-text-align-end oj-sm-padding-4x-start oj-md-4 oj-sm-12">
             <h3 id="activitiesHeader">Activities</h3>
-            <div id="activitiesItems" class="item-display no-wrap">
+            {/* <div id="activitiesItems" class="item-display no-wrap">
                 <ul>
                     <li class="li-item">Baseball</li>
                     <li class="li-item">Bicycling</li>
                     <li class="li-item">Skiing</li>
                     <li class="li-item">Soccer</li>
                 </ul>
-            </div>
+            </div> */}
+
+            <oj-list-view id="activitiesList" class="item-display" aria-labelledby="activitiesHeader"
+                data={props.data}
+                selectionMode={"single"}
+                scrollPolicy={"loadMoreOnScroll"}
+                gridlines={gridlinesItemVisible}
+                scrollPolicyOptions={scrollPolicyOpts}
+            >
+                <template slot={"itemTemplate"} render={listItemRenderer}></template>
+            </oj-list-view>
+
         </div>
     );
 };
--- src/components/ParentContainer1.tsx
+++ src/components/ParentContainer1.tsx
@@ -1,11 +1,25 @@
 import ActivityContainer from "./Activity/ActivityContainer";
 import ParentContainer2 from "./ParentContainer2";
+import MutableArrayDataProvider = require('ojs/ojmutablearraydataprovider');
+import * as storeData from 'text!./store_data.json';
 
+type Activity = {
+    id: number;
+    name: string;
+    short_desc: string;
+};
+
+const activityDataProvider = new MutableArrayDataProvider<Activity['id'], Activity>(
+    JSON.parse(storeData),
+    {
+        keyAttributes: 'id',
+    }
+);
 
 const ParentContainer1 = () => {
     return (
         <div id="parentContainer1" class="oj-flex oj-flex-init oj-panel oj-bg-warning-20">
-            <ActivityContainer />
+            <ActivityContainer data={activityDataProvider} />
             <ParentContainer2 />
         </div>
     );
